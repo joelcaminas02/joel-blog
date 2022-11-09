5cfa89e07f4a16eb9bd58324cdf1a8d4303316b8
joelcaminas02 <joelcg2002@gmail.com>
2022-11-09 10:36:28 +0100

    thankyou

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactFormType;
 
 class PageController extends AbstractController
 {
@@ -24,12 +28,25 @@ class PageController extends AbstractController
         return $this->render('page/about.html.twig', []);
     }
     /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact(): Response
-    {
-        return $this->render('page/contact.html.twig', []);
+ * @Route("/contact", name="contact")
+ */
+public function contact(ManagerRegistry $doctrine, Request $request): Response
+{
+    $contact = new Contact();
+    $form = $this->createForm(ContactFormType::class, $contact);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $contacto = $form->getData();    
+        $entityManager = $doctrine->getManager();    
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+        return $this->redirectToRoute('thankyou', []);
     }
+    return $this->render('page/contact.html.twig', array(
+        'form' => $form->createView()    
+    ));
+}
+
     /**
      * @Route("/class", name="class")
      */
@@ -43,5 +60,12 @@ class PageController extends AbstractController
     public function feature(): Response
     {
         return $this->render('page/feature.html.twig', []);
+    }
+    /**
+     * @Route("/thankyou", name="thankyou")
+     */
+    public function thankyou(): Response
+    {
+        return $this->render('page/thankyou.html.twig', []);
     }
 }
