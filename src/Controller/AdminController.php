fdd7c1923f6e5471675bd30660c33cd3e20243cb
joelcaminas02 <joelcg2002@gmail.com>
2022-11-16 10:22:25 +0100

    categoria fin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+
 
 class AdminController extends AbstractController
 {
@@ -15,9 +20,28 @@ class AdminController extends AbstractController
     }
     
     #[Route('/admin/categories', name: 'app_categories')]
-    public function categories(): Response
-    {
-        return $this->render('admin/categories.html.twig', []);
+public function categories(ManagerRegistry $doctrine, Request $request): Response
+{
+    $repositorio = $doctrine->getRepository(Category::class);
+
+    $categories = $repositorio->findAll();
+
+    $category = new Category();
+    $form = $this->createForm(CategoryFormType::class, $category);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $category = $form->getData();    
+        $entityManager = $doctrine->getManager();    
+        $entityManager->persist($category);
+        $entityManager->flush();
     }
+    return $this->render('admin/categories.html.twig', array(
+        'form' => $form->createView(),
+        'categories' => $categories   
+    ));
+
+}
+
+
 
 }
